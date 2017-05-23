fd2b30d608b19bccad3d0baa061c613aec6b92a1
Radosław Malinowski <malinowski.rad@gmail.com>
2017-05-23 22:12:10 +0200

    fixtures and readme file

---
+++ README.md
@@ -0,0 +1,8 @@
+This repository is built on docker. To start app is enough to use command `docker-compose up` in project directory.
+Server starts on port 8080.
+Example configuration is in .env.dist file, copy it as .env
+
+Database schema is in schema.sql file.
+Example data is in fixtures.sql file.
+
+Api requests are prefixed with: `/api/`
---
+++ fixtures.sql
@@ -0,0 +1,20 @@
+INSERT INTO `database`.leagues (name, description) VALUES ('Test League', 'Example test league');
+INSERT INTO `database`.leagues (name, description) VALUES ('Second Test League', 'Example test league with longer description..................................................................................................................................................');
+INSERT INTO `database`.leagues (name, description) VALUES ('Third Test League', 'Example test league');
+
+
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerB', 'PlayerC', 'PlayerD', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerB', 'PlayerC', 'PlayerE', 1, 2);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerB', 'PlayerD', 'PlayerE', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerC', 'PlayerB', 'PlayerD', 1, 10);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerC', 'PlayerB', 'PlayerE', 10, 20);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerC', 'PlayerD', 'PlayerE', 7, 5);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerD', 'PlayerB', 'PlayerC', 12, 5);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerD', 'PlayerB', 'PlayerE', 20, 5);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerD', 'PlayerC', 'PlayerE', 5, 10);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerE', 'PlayerB', 'PlayerC', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerE', 'PlayerB', 'PlayerD', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (1, 'PlayerA', 'PlayerE', 'PlayerC', 'PlayerD', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (2, 'PlayerA', 'PlayerB', 'PlayerC', 'PlayerD', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (2, 'PlayerA', 'PlayerC', 'PlayerB', 'PlayerD', null, null);
+INSERT INTO `database`.matches (league_id, team_1_player_1, team_1_player_2, team_2_player_1, team_2_player_2, team_1_score, team_2_score) VALUES (2, 'PlayerA', 'PlayerD', 'PlayerB', 'PlayerC', 1, 11);
