19783c13ae00dc84f7952f2bdf2806acccf6cdcd
Radosław Malinowski <malinowski.rad@gmail.com>
2017-05-23 17:41:07 +0200

    db schema draft

---
+++ schema.sql
@@ -0,0 +1,21 @@
+CREATE TABLE leagues
+(
+  name VARCHAR(100) NULL,
+  description VARCHAR(255) NULL,
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT
+    PRIMARY KEY
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `matches`
+(
+  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT
+    PRIMARY KEY,
+  `league_id` INT UNSIGNED NOT NULL,
+  `team_1_player_1` VARCHAR(100) NOT NULL,
+  `team_1_player_2` VARCHAR(100) NOT NULL,
+  `team_2_player_1` VARCHAR(100) NOT NULL,
+  `team_2_player_2` VARCHAR(100) NOT NULL,
+  `team_1_score` INT UNSIGNED NULL,
+  `team_2_score` INT UNSIGNED NULL,
+  FOREIGN KEY (`league_id`) REFERENCES `leagues`(`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
